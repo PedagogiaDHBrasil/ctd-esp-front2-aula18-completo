1c1e8ec2821bdc46b09ebc30cb78d97f7c815525
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-08-17 23:32:31 -0300

    Add files via upload

---
+++ src/features/hooks/useAlert.hook.tsx
@@ -0,0 +1,17 @@
+// Podemos alterar a implementação para nossa própria biblioteca de alertas, graças ao princípio SOLID de inversão de dependência.
+const useAlert = () => {
+  const dispatchSuccessAlert = (message: string) => {
+    alert("SUCCESS: " + message);
+  };
+
+  const dispatchErrorAlert = (message: string) => {
+    alert("ERROR: " + message);
+  };
+
+  return {
+    dispatchSuccessAlert,
+    dispatchErrorAlert,
+  };
+};
+
+export default useAlert;
